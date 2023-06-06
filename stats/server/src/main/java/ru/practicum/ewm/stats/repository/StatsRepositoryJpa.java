fb6e18d476352744009f2c86c1934700cb13b771
kmmins <kmmins@ya.ru>
2023-06-06 15:14:51 +0000

    fix: checkstyle;

--- stats/server/src/main/java/ru/practicum/ewm/stats/repository/StatsRepositoryJpa.java
+++ stats/server/src/main/java/ru/practicum/ewm/stats/repository/StatsRepositoryJpa.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import ru.practicum.ewm.stats.model.HitModel;
-import ru.practicum.ewm.stats.model.StatsModel;
 
 import java.time.LocalDateTime;
 import java.util.List;
